b2862634d85a1a24ffafeb45cd80044284021d82
rickyzatnika <rickyzatnika91@gmail.com>
2023-06-21 05:10:14 +0700

    huahsudhasu

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     throw new Error("Connection failed!");
   }
